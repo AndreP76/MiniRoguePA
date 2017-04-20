88885a9076aea58132830c90b69b50504900e4d5
AndreP76 <andrepereira68@gmail.com>
2017-04-20 13:26:48 +0100

    Stuff

--- src/com/TPPA/GameLogic/Cards/TrapCard.java
+++ src/com/TPPA/GameLogic/Cards/TrapCard.java
@@ -1,8 +1,6 @@
 package com.TPPA.GameLogic.Cards;
 
-import com.TPPA.GameLogic.AwaitCardSelectionState;
-import com.TPPA.GameLogic.IState;
-import com.TPPA.GameLogic.Main;
+import com.TPPA.GameLogic.*;
 
 /**
  * Created by andre on 4/19/17.
@@ -15,6 +13,64 @@ public class TrapCard extends CardBase {
     @Override
     public IState Effect() {
         Main.ErrorStream.println("TrapCard effect called!");
+        Dice d = new Dice();
+        Player P = GameStateController.getCurrentController().getCurrentPlayer();
+        switch (d.Roll()) {
+            case 1: {
+                if (P.getFood() >= 1) {
+                    P.incFood(-1);
+                } else {
+                    P.incHP(-2);
+                }
+                Main.ErrorStream.println("Mold Miasma");
+                break;
+            }
+
+            case 2: {
+                if (P.getGold() >= 1) {
+                    P.incGold(-1);
+                } else {
+                    P.incHP(-2);
+                }
+                Main.ErrorStream.println("Tripwire");
+                break;
+            }
+            case 3: {
+                if (P.getArmor() >= 1) {
+                    P.incArmor(-1);
+                } else {
+                    P.incHP(-2);
+                }
+                Main.ErrorStream.println("Acid Mist");
+                break;
+            }
+            case 4: {
+                P.incHP(-1);
+                Main.ErrorStream.println("Spring blades");
+                break;
+            }
+            case 5: {
+                if (P.getXP() >= 1) {
+                    P.incXP(-1);
+                } else {
+                    P.incHP(-2);
+                }
+                Main.ErrorStream.println("Moving Walls");
+                break;
+            }
+            case 6: {
+                P.incHP(-2);
+                Main.ErrorStream.println("Player lost 2 HP");
+                GameStateController GSC = GameStateController.getCurrentController();
+                if (GSC.getCurrentRoom() == GSC.getRoomsInZone() && GSC.getCurrentZone() == GSC.getMaxZones()) {
+                    Main.ErrorStream.println("Player cannot fall anymore.");
+                } else {
+                    //TODO : Some weird math happens here
+                }
+            }
+            default:
+                break;
+        }
         return new AwaitCardSelectionState();
     }
 }
--- src/com/TPPA/GameLogic/TradingState.java
+++ src/com/TPPA/GameLogic/TradingState.java
@@ -1,5 +1,10 @@
 package com.TPPA.GameLogic;
 
+import com.TPPA.GameLogic.Spells.FireSpell;
+import com.TPPA.GameLogic.Spells.HealSpell;
+import com.TPPA.GameLogic.Spells.IceSpell;
+import com.TPPA.GameLogic.Spells.PoisonSpell;
+
 /**
  * Created by andre on 4/5/17.
  */
@@ -7,6 +12,78 @@ public class TradingState extends GameState {
 
     @Override
     public Action[] GetActions() {
-        return new Action[0];
+        Action[] Act = new Action[8];
+        Act[0] = new Action(InternalCommandsDictionary.BuyRation, "Comprar comida: +1 Food / -1 Gold");
+        Act[1] = new Action(InternalCommandsDictionary.BuyHealthPotion, "Comprar poção de vida: +1 HP / -1 Gold");
+        Act[2] = new Action(InternalCommandsDictionary.BuyBigHealthPotion, "Comprar poção de vida grande: +4 HP / -3 Gold");
+        Act[3] = new Action(InternalCommandsDictionary.BuyArmorPiece, "Comprar peça de armadura: +1 Armor / -6 Gold");
+        Act[4] = new Action(InternalCommandsDictionary.BuySpell, "Comprar feitiço: +1 Spell aleatório / -8 Gold");
+        Act[5] = new Action(InternalCommandsDictionary.SellArmorPiece, "Vender peça de armadura: -1 Armor / +3 Gold");
+        Act[6] = new Action(InternalCommandsDictionary.SellSpell, "Vender feitiço: -1 Spell / +4 Gold");
+        Act[7] = new Action(InternalCommandsDictionary.EndTradingState, "Passar à fase seguinte");
+        return Act;
+    }
+
+    @Override
+    public IState Action(String ActionString) {
+
+        if (ActionString.equals(InternalCommandsDictionary.BuyRation)) {
+            GameStateController.getCurrentController().getCurrentPlayer().incFood(1);
+            GameStateController.getCurrentController().getCurrentPlayer().incGold(-1);
+            return this;
+        }
+        if (ActionString.equals(InternalCommandsDictionary.BuyHealthPotion)) {
+            GameStateController.getCurrentController().getCurrentPlayer().incHP(1);
+            GameStateController.getCurrentController().getCurrentPlayer().incGold(-1);
+            return this;
+        }
+        if (ActionString.equals(InternalCommandsDictionary.BuyBigHealthPotion)) {
+            GameStateController.getCurrentController().getCurrentPlayer().incHP(4);
+            GameStateController.getCurrentController().getCurrentPlayer().incGold(-3);
+            return this;
+        }
+        if (ActionString.equals(InternalCommandsDictionary.BuyArmorPiece)) {
+            GameStateController.getCurrentController().getCurrentPlayer().incArmor(1);
+            GameStateController.getCurrentController().getCurrentPlayer().incGold(-6);
+            return this;
+        }
+        if (ActionString.equals(InternalCommandsDictionary.BuySpell)) {
+            giveSpell();
+            GameStateController.getCurrentController().getCurrentPlayer().incGold(-8);
+            return this;
+        }
+        if (ActionString.equals(InternalCommandsDictionary.SellArmorPiece)) {
+            GameStateController.getCurrentController().getCurrentPlayer().incArmor(-1);
+            GameStateController.getCurrentController().getCurrentPlayer().incGold(3);
+            return this;
+        }
+        if (ActionString.equals(InternalCommandsDictionary.SellSpell)) {
+            //TODO: escolher feitiço para vender
+            GameStateController.getCurrentController().getCurrentPlayer().incGold(4);
+            return this;
+        }
+        if (ActionString.equals(InternalCommandsDictionary.EndTradingState)) {
+            return new AwaitCardSelectionState();
+        }
+        return this;
+    }
+
+    public void giveSpell() {
+        int id = (int) (Math.random() % 4);
+
+        switch (id) {
+            case 0:
+                GameStateController.getCurrentController().getCurrentPlayer().getSpellsInventory().add(new FireSpell(InternalCommandsDictionary.FireSpellID));
+                break;
+            case 1:
+                GameStateController.getCurrentController().getCurrentPlayer().getSpellsInventory().add(new HealSpell(InternalCommandsDictionary.HealSpellID));
+                break;
+            case 2:
+                GameStateController.getCurrentController().getCurrentPlayer().getSpellsInventory().add(new IceSpell(InternalCommandsDictionary.IceSpellID));
+                break;
+            case 3:
+                GameStateController.getCurrentController().getCurrentPlayer().getSpellsInventory().add(new PoisonSpell(InternalCommandsDictionary.PoisonSpellID));
+                break;
+        }
     }
 }
